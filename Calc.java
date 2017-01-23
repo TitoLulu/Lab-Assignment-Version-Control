0d7dc5ffcbe579501e1b8b647c1a325e168def25
TitoLulu <titolulu6@gmail.com>
2017-01-23 22:52:45 +0000

    deleted sub functionality

---
+++ Calc.java
@@ -0,0 +1,12 @@
+public class Calc{
+	public static void main(String[] args){
+		if(args[0].equalsIgnoreCase("add")){
+			double num1= Double.parseDouble(args[1]);
+			double num2= Double.parseDouble(args[2]);
+			System.out.println(num1+num2);
+		}
+
+		
+	}
+}
+
